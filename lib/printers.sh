2202767c0f8c64816791c9b4bad911fb77e9e1df
Keith Craley <kcral21@gmail.com>
2018-05-10 21:47:45 -0400

    Adding colors and printer helpers

---
+++ lib/printers.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Colors to be used for the printers
+
+# Colors
+BLINK="\e[5m"
+COL_RESET="\e[0m"
+COL_RED="\e[31m"
+COL_GREEN="\e[32m"
+COL_YELLOW="\e[33m"
+COL_BLUE="\e[34m"
+COL_MAGENTA="\e[35m"
+COL_CYAN="\e[36m"
+COL_LIGHT_GREY="\e[37m"
+COL_DARK_GREY="\e[90m"
+COL_LIGHT_RED="\e[91m"
+COL_LIGHT_GREEN="\e[92m"
+COL_LIGHT_YELLOW="\e[93m"
+COL_LIGHT_BLUE="\e[94m"
+COL_LIGHT_MAGENTA="\e[95m"
+COL_LIGHT_CYAN="\e[96m"
+
+function action() {
+  echo -e "$COL_BLUE[action]:$COL_RESET "$1
+}
+
+function error() {
+  echo -e "$COL_RED[error]:$COL_RESET "$1
+}
+
+function ok() {
+  echo -e "$COL_GREEN[ok]:$COL_RESET "$1
+}
+
+function warn() {
+  echo -e "$COL_YELLOW[error]:$COL_RESET "$1
+}
