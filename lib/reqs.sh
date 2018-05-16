e9ec3ede9c36a3c778b3234fc3bc3c13a3ad8b57
Keith Craley <kcral21@gmail.com>
2018-05-15 20:14:09 -0400

    Adding node install function

--- lib/reqs.sh
+++ lib/reqs.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
 # Requirements for setting up dotfiles
+
+function require_node() {
+    running "node -v"
+    node -v
+    if [ $? != 0 ]; then
+        action "Node not found, install via package manager"
+        curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+        sudo apt-get install -y nodejs
+    fi
+}
