56ba4912e9fd2a2b9bee7e97dc7bc6a5148c3f8e
Keith Craley <kcral21@gmail.com>
2018-05-15 21:31:14 -0400

    Adding Oh-My-Zsh installation functionality

--- lib/reqs.sh
+++ lib/reqs.sh
@@ -4,9 +4,25 @@
 function require_node() {
     running "node -v"
     node -v
-    if [ $? != 0 ]; then
-        action "Node not found, install via package manager"
+    if [[ $? != 0 ]]; then
+        warning "Node not found, install via package manager"
+        running "curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -"
         curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+        running "sudo apt-get install -y nodejs"
         sudo apt-get install -y nodejs
+    else
+        running "Node is already installed"
+        ok
+    fi
+}
+
+function require_ohmyzsh() {
+    running "Installing Oh-My-Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    if [[ $? != 0 ]]; then
+        ok
+    else
+        error
+        exit 1
     fi
 }
