f32e9a9fda3fd720effb4c7dbe644489b9b0e05f
Keith Craley <kcral21@gmail.com>
2021-05-22 04:09:34 -0400

    add uninstall boilerplate

--- lib/uninstall.sh
+++ lib/uninstall.sh
@@ -3,3 +3,17 @@
 # This script contains all logic necessary to uninstall
 # moar dots, all dotfiles and configurations made.
 
+# This is the main uninstall function which is called
+# when `./moar_dots.sh -u` is executed.
+function uninstall() {
+    action "We are going to uninstall the dotfiles from moar_dots"
+    prompt "Would you like to continue? [y|n]: " && read response
+    if [[ $response =~ (yes|y|Y) ]]; then
+	action "Removing dotfiles and custom configuration!"
+    else
+	action "Aborting uninstallation"
+	exit 0
+    fi
+
+    action "Beginning uninstall"
+}
