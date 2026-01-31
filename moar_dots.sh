79394a8ff3d6465739090854ced8ac9143129f7a
Keith Craley <kcral21@gmail.com>
2026-01-31 01:02:10 -0500

    remove old vim variables

--- moar_dots.sh
+++ moar_dots.sh
@@ -14,8 +14,6 @@ export XDG_DATA_HOME=${XDG_DATA_HOME:=~/.local/share}
 
 export HOME_BIN_DIR=${HOME_BIN_DIR:=~/bin}
 export HOME_ENV_DIR=${HOME_ENV_DIR:=~/.env}
-export VIM_AUTOLOAD_DIR=${VIM_AUTOLOAD_DIR:=~/.vim/autoload}
-export VIM_BUNDLE_DIR=${VIM_BUNDLE_DIR:=~/.vim/bundle}
 
 export TFENV_DIR=${TFENV_DIR:=~/.tfenv}
 export FZF_DIR=${FZF_DIR:=~/.fzf}
